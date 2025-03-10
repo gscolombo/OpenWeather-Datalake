102ca7d85133bb538ec46b63061f99302b6a6564
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-03-09 23:19:39 -0300

    Teste de integração entre ingestão de dados brutos e processamento inicial

--- src/silver/raw_data_processor.py
+++ src/silver/raw_data_processor.py
@@ -2,6 +2,7 @@ import os
 
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import lit, from_unixtime
+from pyspark.sql.streaming.query import StreamingQuery
 from functools import reduce
 from .raw_data_schema import schema
 
@@ -10,12 +11,15 @@ ROOT_PATH = os.path.abspath(__file__).split("/src")[0]
 
 class RawDataProcessor:
 
-    def __init__(self):
+    def __init__(self, root: str = ROOT_PATH):
+        self.root = root
+        self.path = f"{root}/data/bronze/weather"
+        assert os.path.exists(self.path)
+
         self.spark = SparkSession.builder.appName(
             "Raw Data Initial Processing"
         ).getOrCreate()
 
-        self.path = f"{ROOT_PATH}/data/bronze/weather"
         self.capitals = os.listdir(self.path)
 
     def read_json(self, path: str):
@@ -55,14 +59,19 @@ class RawDataProcessor:
 
         return reduce(lambda a, b: a.union(b), climate_data)
 
-    def save_climate_data_to_parquet(self):
-        save_path = f"{ROOT_PATH}/data/silver/climate"
+    def save_climate_data_to_parquet(self) -> StreamingQuery:
+        save_path = f"{self.root}/data/silver/climate"
         if not os.path.exists(save_path):
-            os.mkdir(save_path)
+            os.makedirs(save_path, mode=0o777)
             os.mkdir(save_path + "/checkpoint")
 
         climate_data = self.get_climate_data()
 
-        climate_data.writeStream.trigger(availableNow=True).format("parquet").option(
-            "checkpointLocation", save_path + "/checkpoint"
-        ).start(save_path)
+        q = (
+            climate_data.writeStream.trigger(availableNow=True)
+            .format("parquet")
+            .option("checkpointLocation", save_path + "/checkpoint")
+            .start(save_path)
+        )
+
+        return q
--- tests/test_initial_raw_data_processing.py
+++ tests/test_initial_raw_data_processing.py
@@ -15,17 +15,20 @@ schema = expected_data_schema
 
 
 @pytest.fixture
-def raw_data_processor():
-    return RawDataProcessor()
+def sample_data_path(tmp_path):
+    return f"{tmp_path}{os.sep}data{os.sep}bronze{os.sep}weather{os.sep}Manaus"
 
 
 class TestInitialRawDataProcessing:
 
-    def test_climate_data_gathering(self, raw_data_processor, sample, tmp_path, schema):
-        with open(f"{tmp_path}/sample_data.json", "w") as json:
+    def test_climate_data_gathering(self, sample_data_path, tmp_path, sample, schema):
+        os.makedirs(sample_data_path)
+        with open(f"{sample_data_path}/sample_data.json", "w") as json:
             dump(sample, json)
 
-        sample_raw_data_df = raw_data_processor.read_json(str(tmp_path))
+        raw_data_processor = RawDataProcessor(tmp_path)
+
+        sample_raw_data_df = raw_data_processor.read_json(str(sample_data_path))
 
         transformed_raw_data_df = raw_data_processor.get_climate_data_per_capital(
             sample_raw_data_df, "Manaus"
@@ -54,5 +57,4 @@ class TestInitialRawDataProcessing:
             expected_data, schema=schema
         )
 
-        expected_df.show()
         assertDataFrameEqual(transformed_raw_data_df, expected_df)
