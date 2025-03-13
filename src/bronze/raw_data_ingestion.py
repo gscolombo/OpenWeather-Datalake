f09ffa105e93a376b92dab02994fcac899cc332f
Gabriel S. Colombo <gscolombo404@gmail.com>
2025-03-12 21:29:36 -0300

    Refatoração de definição de variável para caminho de pasta de destino de dados brutos

--- src/bronze/raw_data_ingestion.py
+++ src/bronze/raw_data_ingestion.py
@@ -34,11 +34,9 @@ def request_data(capital: str):
     return {"status_code": res.status_code, "data": res.json()}
 
 
-def save_data(data: dict, capital: str, root: str = "."):
+def save_data(data: dict, capital: str, root: str = ""):
     now = datetime.now().strftime("%Y%m%d_%H%M%S")
-    save_path = Path(
-        f"{root}{os.sep}data{os.sep}bronze{os.sep}weather{os.sep}{capital}"
-    )
+    save_path = Path(root, "data", "bronze", "weather", capital)
 
     if not save_path.exists():
         save_path.mkdir(parents=True, exist_ok=True)
